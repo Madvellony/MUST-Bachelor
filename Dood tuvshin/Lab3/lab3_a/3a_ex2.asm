18d3eb5a9ea46f79e311bd3ece4a080a47e8fdd5
Tushigt <63115350+vacation-2@users.noreply.github.com>
2024-04-17 13:33:59 +0800

    Delete Dood tuvshin directory

--- Dood tuvshin/Lab3/lab3_a/3a_ex2.asm
+++
@@ -1,6 +0,0 @@
-main:
-	ori $8, $0, 12
-	sll $9, $8, 2 #register 8 iin utgiig x4
-	addu $9, $9, $8 #register 9 deer x5
-	addiu $9, $9, -74
-	
--- Dood tuvshin/Lab3/lab3_b/lab3b_ex1.asm
+++
@@ -1,6 +0,0 @@
-main:
-	ori $8, $0, 12
-	ori $9, $0, 5
-	mult $9, $8
-	mflo $9
-	addiu $9, $9, -74
